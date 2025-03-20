c3d19ec84f9c5b2877fb216d6feeb9a1e0bfa596
Vlado0sina <vladgod23@gmail.com>
2025-03-20 15:34:51 +0000

    Created Node class for linked list implementation in priority queue. The PriorityItem has a reference to the next node.

--- PriorityQueue/PriorityItem.cs
+++ PriorityQueue/PriorityItem.cs
@@ -1,4 +1,6 @@
-﻿namespace PriorityQueue
+﻿using System.Configuration;
+
+namespace PriorityQueue
 {
     public class PriorityItem<T>
     {
@@ -14,6 +16,22 @@
         public override string ToString()
         {
             return $"({Item}, {Priority})";
+        }
+    }
+
+    //Represents a node in a linked list for a priority queue
+    public class Node<T>
+    {
+        //Gets or sets the priority item is sorted in this node
+        public PriorityItem<T> PriorityItem { get; set; }
+        //Gets or sets the reference to the next node in the list
+        public Node<T> Next { get; set; }
+
+        //Constructor
+        public Node(PriorityItem<T> priorityItem)
+        {
+            PriorityItem = priorityItem;
+            Next = null;
         }
     }
 }
